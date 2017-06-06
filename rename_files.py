d309c8e8382f6147d435572def081d5fdd8c7861
KamikidFu <kidfu@live.com>
2017-06-06 04:19:36 +0000

    Finished rename file function

--- rename_files.py
+++ rename_files.py
@@ -1,9 +1,15 @@
 import os
+import 
 
 def rename_files():
+	saved_path = os.getcwd()
 	#1 get file names from a folder
 	file_list = os.listdir("/home/vagrant/udacity/pyBase/pics")
-	print(file_list)
+	#print(file_list)
 	#2 for each file, rename it
+	os.chdir("/home/vagrant/udacity/pyBase/pics")
+	for file_name in file_list:
+		os.rename(file_name,file_name.translate(Node, "0123456789"))
+	os.chdir(saved_path)
 
 rename_files()
