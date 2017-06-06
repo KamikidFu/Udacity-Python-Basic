d8b859ad58089afcb609b056cd49d86b5ff4e658
Kamikid <kidfu@live.com>
2017-06-06 18:00:54 +1200

    Fix typo and spaces

--- drawSquare.py
+++ drawSquare.py
@@ -4,21 +4,24 @@ def draw_square():
 	window = turtle.Screen()
 	window.bgcolor("red")
 
-	brad = tutle.Turtle()
-	brad.forward(100)
-	brad.right(90)
+	brad = turtle.Turtle()
+	brad.shape("turtle")
+	brad.color("lime")
+	brad.speed(2)
 	
 	brad.forward(100)
-        brad.right(90)
-
+	brad.right(90)
 	brad.forward(100)
-        brad.right(90)
-
+	brad.right(90)
 	brad.forward(100)
-        brad.right(90)
-
+	brad.right(90)
 	brad.forward(100)
-        brad.right(90)
+	brad.right(90)
+
+	angie = turtle.Turtle()
+	angie.shape("arrow")
+	angie.color("blue")
+	angie.circle(25)
 
 	window.exitonclick()
 
