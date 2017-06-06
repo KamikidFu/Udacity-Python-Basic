fc2f7d45abbab8d860a04ef84df3a6a18fe4410b
KamikidFu <kidfu@live.com>
2017-06-06 03:41:37 +0000

    Add while loop to do 3 times

--- break_time.py
+++ break_time.py
@@ -1,6 +1,13 @@
 import webbrowser
 import time
 #Using for loop
-for i in range(3):
+#for i in range(3):
+#	time.sleep(10)
+#	webbrowser.open("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
+total_breaks = 3
+break_count = 0
+print("This program started on" + time.ctime())
+while(break_count<total_breaks):
 	time.sleep(10)
 	webbrowser.open("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
+	break_count = break_count + 1
