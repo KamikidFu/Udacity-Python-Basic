34e5f7cbf84b681537977260a29b36493dda052c
KamikidFu <kidfu@live.com>
2017-06-07 07:16:37 +0000

    Add movie website program

---
+++ entertainment_center.py
@@ -0,0 +1,8 @@
+import media
+
+toy_story = media.Movie("Toy Story","Storyline", 
+		"http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg", 
+		"https://www.youtube.com/watch?v=vwyZH85NQC4")
+
+print(toy_story.storyline)
+toy_story.show_trailer()
