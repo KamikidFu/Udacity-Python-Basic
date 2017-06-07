da7d03605008b883bc3c58bfbbbd6c1d4a6aba1b
KamikidFu <kidfu@live.com>
2017-06-07 07:42:09 +0000

    Add class variable for rating

--- media.py
+++ media.py
@@ -1,7 +1,8 @@
 import webbrowser
 class Movie():
-	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-		self.title = movie_title
+	VALID_RATINGS = ["G","PG","PG-13","R"] #Class variables
+	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube): #Constructor
+		self.title = movie_title	#Instance variables
 		self.storyline = movie_storyline
 		self.poster_image_url = poster_image
 		self.trailer_youtube_url = trailer_youtube
