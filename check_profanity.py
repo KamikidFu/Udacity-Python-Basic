efbd2bd19a60f012508ec3af38d6968bbf9ff93f
KamikidFu <kidfu@live.com>
2017-06-07 05:37:20 +0000

    Output in a better way

--- check_profanity.py
+++ check_profanity.py
@@ -3,14 +3,18 @@ import urllib
 def read_text(URL):
 	quotes = open(URL)
 	contents_of_file = quotes.read()
-	print(contents_of_file)
+	#print(contents_of_file)
 	quotes.close()
 	check_profanity(contents_of_file)
 
 def check_profanity(key):
 	connection = urllib.urlopen("http://www.wdylike.appspot.com/?q="+ key)
 	output = connection.read()
-	print(output)
+	#print(output)
+	if(output=="true"):
+		print("Alert!")
+	else:
+		print("Passed!")
 	connection.close()
 
 read_text("/home/vagrant/udacity/pyBase/test_profanity.txt")
